b9d6bc0ae2393c4c8c606fa3a136b94a309bf349
siva <trailnew112@gmail.com>
2022-12-01 10:47:21 +0000

    Match Game

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Gallery from './components/Gallery'
+import './App.css'
+
+// These are the lists used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => <Gallery />
+
+export default App
